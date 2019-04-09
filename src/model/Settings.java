26c75d44c9b901c466ab22ff7d95b05ee78fffa3
BloodBaron <barontm@gmail.com>
2019-04-09 17:56:35 +0200

    Now time for settings

---
+++ src/model/Settings.java
@@ -0,0 +1,21 @@
+package model;
+
+import javax.swing.text.html.CSS;
+
+public class Settings {
+	
+	private CSS background;
+	
+	
+	
+	public Settings() {
+		background = new CSS();
+		
+	}
+	
+	public void reset() {
+		background.
+		
+	}
+
+}
